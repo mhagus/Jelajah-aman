3a41d24e7c3406541e0ac536cd58999ca52fa9b7
Muhammad Agus <muhammad.agus24026@mhs.uingusdur.ac.id>
2026-09-15 15:40:50 +0700

    feat: tambahkan IndikatorAQI di halaman Beranda

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,26 +5,31 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import WeatherCard from "../../components/WeatherCard";
 import SearchBox from "../../components/SearchBox";
 import RiwayatList from "../../components/RiwayatList";
+import IndikatorAQI from "../../components/IndikatorAQI";
+
 export default function HalamanUtama() {
- const [kotaAktif, setKotaAktif] = useState("Pekalongan");
- const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
- const { width } = useWindowDimensions();
- const isTablet = width > 768;
- useEffect(() => {
- console.log("Kota aktif berubah menjadi:", kotaAktif);
-    }, [kotaAktif]);
-    function handleCari(kota: string) {
+  const [kotaAktif, setKotaAktif] = useState("Pekalongan");
+  const [riwayat, setRiwayat] = useState<string[]>(["Pekalongan"]);
+  const { width } = useWindowDimensions();
+  const isTablet = width > 768;
+
+  useEffect(() => {
+    console.log("Kota aktif berubah menjadi:", kotaAktif);
+  }, [kotaAktif]);
+
+  function handleCari(kota: string) {
     setKotaAktif(kota);
     if (!riwayat.includes(kota)) {
-    setRiwayat([...riwayat, kota]);
+      setRiwayat([...riwayat, kota]);
     }
- }
- 
- return (
- <SafeAreaView style={{ flex: 1, padding: isTablet ? 32 : 16, gap: 16 }}>
- <SearchBox onCari={handleCari} />
- <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
- <RiwayatList daftarKota={riwayat} />
- </SafeAreaView>
- );
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1, padding: isTablet ? 32 : 16, gap: 16 }}>
+      <SearchBox onCari={handleCari} />
+      <WeatherCard kota={kotaAktif} suhu={29} tingkatAQI="BAIK" />
+      <IndikatorAQI kota={kotaAktif} indeksAQI={35} tingkat="BAIK" diperbaruiPada="Hari ini, 15:00" />
+      <RiwayatList daftarKota={riwayat} />
+    </SafeAreaView>
+  );
 }
